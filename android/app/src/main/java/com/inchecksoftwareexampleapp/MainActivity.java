db417b5fe721bab86ba9ac9274da9e3780c6e649
Douglas Wissett Walker <douglas.wissett@gmail.com>
2020-06-08 09:58:54 +0100

    Created react-native app & installed wix/react-native-navigation

---
+++ android/app/src/main/java/com/inchecksoftwareexampleapp/MainActivity.java
@@ -0,0 +1,8 @@
+package com.inchecksoftwareexampleapp;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+  
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * @format
+ */
+import {Navigation} from 'react-native-navigation';
+// import {AppRegistry} from 'react-native';
+import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(
+  'com.inchecksoftwareexampleapp.WelcomeScreen',
+  () => App,
+);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.inchecksoftwareexampleapp.WelcomeScreen',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
